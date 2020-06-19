c5555f062db8bb06451a9c3c20e6610f65fab3fe
yui-nkym <yui.nakayama@di-v.co.jp>
2020-06-19 15:45:36 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,21 @@
 class MessagesController < ApplicationController
   def index
+    @message = Message.new
+    @room = Room.find(params[:room_id])
+    @messages = @room.messages.includes(:user)
+  end
+
+  def create
+    @room = Room.find(params[:room_id])
+    @message = @room.messages.new(message_params)
+    if @message.save
+      redirect_to room_messages_path(@room)
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:text).merge(user_id: current_user.id)
   end
 end
