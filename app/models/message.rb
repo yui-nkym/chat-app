dca75a9a21ea863379eacbc44ff49d4526fb7e70
yui-nkym <yui.nakayama@di-v.co.jp>
2020-06-19 16:15:06 +0900

    空のメッセージが送信できないバリデーションの設定

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,5 @@
 class Message < ApplicationRecord
   belongs_to :room
   belongs_to :user
+  validates :text, presence: true
 end
--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,7 @@ module ChatApp
       g.helper false
       g.test_framework false
     end
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
   end
 end
