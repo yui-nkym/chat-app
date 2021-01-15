33c193729b9e30e3611dae1a56ec0f00334e5fcb
yui-nkym <yui.nakayama@di-v.co.jp>
2021-01-15 17:24:27 +0900

    aa

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -3,9 +3,8 @@ FactoryBot.define do
     text {Faker::Lorem.sentence}
     association :user
     association :room
-
+  end
     after(:build) do |message|
       message.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
     end
-  end
 end
